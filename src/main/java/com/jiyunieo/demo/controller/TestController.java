5ee6c00f0554fe6dbad25ff9d749be0fa4078529
jiyun <kjy32697721@gmail.com>
2023-11-22 20:18:01 +0900

    지윤^~^

--- src/main/java/com/jiyunieo/demo/controller/TestController.java
+++ src/main/java/com/jiyunieo/demo/controller/TestController.java
@@ -1,5 +1,17 @@
 package com.jiyunieo.demo.controller;
 
-public class TestController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 
+@Controller
+public class TestController {
+    @GetMapping
+    public String hello() {
+        return "hello";
+    }
+    @GetMapping("/name")
+    public String name() {
+        return "name";
+    }
 }
